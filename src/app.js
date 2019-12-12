9b26b2e64e4de2fd2cf10287bd5d4b5801750c49
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-12-12 16:59:18 -0300

    add cors and alter port

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv';
 import './database'
+import cors from 'cors';
+
 import routes from './routes';
 class App {
   constructor() {
@@ -8,6 +10,7 @@ class App {
     this.routes();
   }
   routes() {
+    this.server.use(cors())
     this.server.use(express.json());
     this.server.use(routes);
   }
